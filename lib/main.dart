ce9ad9001e1c4da107147b813518577208745f7e
khangzxrr <khangzxrr@gmail.com>
2023-07-26 15:33:46 +0700

    add firebase deploy

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:hui_management/provider/authentication_provider.dart';
@@ -11,12 +12,17 @@ import 'package:hui_management/view/dashboard_view.dart';
 import 'package:hui_management/view/login_view.dart';
 import 'package:provider/provider.dart';
 
+import 'firebase_options.dart';
 import 'view/fund/funds_view.dart';
 import 'view/member/members_view.dart';
 import 'view/payments/payment_summaries_view.dart';
 import 'view/payments/payments_members_view.dart';
 
 void main() {
+  Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   SetupService.setup();
 
   runApp(MultiProvider(
@@ -38,7 +44,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Quản lí hụi',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
