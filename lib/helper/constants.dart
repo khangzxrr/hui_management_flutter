34ea917c2fe5198df93be27fbda0bc419270d66f
khangzxrr <khangzxrr@gmail.com>
2023-08-18 03:54:32 +0700

    add token

--- lib/helper/constants.dart
+++ lib/helper/constants.dart
@@ -1,9 +1,12 @@
 class Constants {
   //static const String apiHostName = "https://hui-container.calmground-37e371eb.southeastasia.azurecontainerapps.io";
-  static const String apiHostName = "http://localhost:57679";
+  //static const String apiHostName = "http://localhost:57679";
+  static const String apiHostName = "http://10.0.2.2:57679";
 
   static const defaultAvatarPath = 'user.jpg';
   static const randomAvatarPath = 'https://picsum.photos/200';
 
   static const smallScreenSize = 600;
+
+  static const String webPushNotificationToken = 'BNGA9XwGd7qCq6VkXXVr0g8Dy-RiQJnv8h-TuPJSLJ50y-H_Mhaizu69Dla-twR00dAB2vIqStua3JGnSXTNsN8';
 }
