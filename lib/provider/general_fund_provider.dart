5f6f659bd7787c5666e632e32f1a6d00362bc30d
khangzxrr <khangzxrr@gmail.com>
2023-12-30 12:47:24 +0700

    add pagingState in provider

--- lib/provider/general_fund_provider.dart
+++ lib/provider/general_fund_provider.dart
@@ -3,14 +3,30 @@ import 'package:fpdart/fpdart.dart';
 import 'package:get_it/get_it.dart';
 import 'package:hui_management/model/general_fund_model.dart';
 import 'package:hui_management/service/fund_service.dart';
+import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
+
+import '../helper/constants.dart';
 
 class GeneralFundProvider with ChangeNotifier {
   List<GeneralFundModel> _funds = [];
+
+  PagingState<int, GeneralFundModel> _pagingState = const PagingState<int, GeneralFundModel>(nextPageKey: 0);
 
   void setFunds(List<GeneralFundModel> funds) {
     _funds = funds;
 
     notifyListeners();
+  }
+
+  GeneralFundModel getGeneralFundById(int id) {
+    return getFunds.where((f) => f.id == id).first;
+  }
+
+  void updateGeneralFundMemberCount(int id, int membersCountOffset) {
+    final fund = _funds.where((f) => f.id == id).first;
+    fund.membersCount += membersCountOffset;
+
+    updateFund(fund);
   }
 
   void updateFund(GeneralFundModel fund) {
@@ -35,12 +51,25 @@ class GeneralFundProvider with ChangeNotifier {
   TaskEither<String, List<GeneralFundModel>> fetchFunds(int pageIndex, int pageSize) => TaskEither.tryCatch(() async {
         final funds = await GetIt.I<FundService>().getAll(pageIndex, pageSize);
 
-        setFunds(funds);
+        _funds.addAll(funds);
+
+        if (funds.length < pageSize) {
+          _pagingState = PagingState<int, GeneralFundModel>(itemList: _funds);
+        } else {
+          _pagingState = PagingState<int, GeneralFundModel>(nextPageKey: pageIndex + 1, itemList: funds);
+        }
+
+        notifyListeners();
 
         return funds;
       }, (error, stackTrace) => error.toString());
 
-  List<GeneralFundModel> getFunds() {
-    return _funds;
+  List<GeneralFundModel> get getFunds => _funds;
+
+  PagingState<int, GeneralFundModel> get pagingState => _pagingState;
+
+  Future<void> refreshPagingState() async {
+    _funds.clear();
+    await fetchFunds(0, Constants.pageSize).run();
   }
 }
