d5665278f3a10f70405ca35114c92f25040c18df
khangzxrr <khangzxrr@gmail.com>
2024-01-01 13:42:33 +0700

    add taskEitherRefresh

--- lib/provider/abstract_provider/paginated_provider.dart
+++ lib/provider/abstract_provider/paginated_provider.dart
@@ -4,6 +4,8 @@ import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 abstract class PaginatedProvider<T> {
   PagingState<int, T> pagingState = const PagingState();
 
+  TaskEither<String, void> refreshPagingTaskEither() => TaskEither.tryCatch(() async => await refreshPagingState(), (error, stackTrace) => error.toString());
+
   Future<void> refreshPagingState([String? newSearchTerm]);
 
   TaskEither<String, void> fetchData(int pageIndex, int pageSize, String searchTerm);
