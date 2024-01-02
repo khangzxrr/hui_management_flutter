58a4abf3582ddea0f1bf8ad1b77da6c08db9b9dd
khangzxrr <khangzxrr@gmail.com>
2024-01-02 10:04:26 +0700

    remember to set growable, simplify subuser provider

--- lib/provider/abstract_provider/paginated_provider.dart
+++ lib/provider/abstract_provider/paginated_provider.dart
@@ -1,12 +1,23 @@
 import 'package:fpdart/fpdart.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 
+import '../../helper/constants.dart';
+
 abstract class PaginatedProvider<T> {
   PagingState<int, T> pagingState = const PagingState();
+  List<T> items = List.empty(growable: true);
+  String searchTerm = '';
 
   TaskEither<String, void> refreshPagingTaskEither() => TaskEither.tryCatch(() async => await refreshPagingState(), (error, stackTrace) => error.toString());
 
-  Future<void> refreshPagingState([String? newSearchTerm]);
+  Future<void> refreshPagingState([String? newSearchTerm]) async {
+    if (newSearchTerm != null) {
+      searchTerm = newSearchTerm;
+    }
+
+    items.clear();
+    await fetchData(0, Constants.pageSize, searchTerm).run();
+  }
 
   TaskEither<String, void> fetchData(int pageIndex, int pageSize, String searchTerm);
 }
--- lib/provider/sub_users_provider.dart
+++ lib/provider/sub_users_provider.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fpdart/fpdart.dart';
 import 'package:get_it/get_it.dart';
-import 'package:hui_management/helper/constants.dart';
 import 'package:hui_management/model/sub_user_model.dart';
-import 'package:hui_management/model/sub_user_with_payment_report.dart';
 import 'package:hui_management/provider/abstract_provider/paginated_provider.dart';
 import 'package:hui_management/service/user_service.dart';
 import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
@@ -11,37 +9,6 @@ import 'package:infinite_scroll_pagination/infinite_scroll_pagination.dart';
 enum SubUserFilter { filterByAnyPayment, getFundRatio, filterByNotFinishedPayment, filterByContainToDayPayment }
 
 class SubUsersProvider extends PaginatedProvider<SubUserModel> with ChangeNotifier {
-  final List<SubUserModel> _subUsers = [];
-  String _searchTerm = '';
-  List<SubUserWithPaymentReport> subUsersWithPaymentReport = [];
-
-  // TaskEither<String, List<SubUserModel>> fetchAndFilterUsers(Set<SubUserFilter> filters) => TaskEither.tryCatch(() async {
-  //       final users = await GetIt.I<UserService>().getAll(filters);
-
-  //       return users;
-  //     }, (error, stackTrace) => error.toString());
-
-  TaskEither<String, List<SubUserWithPaymentReport>> getAllWithPaymentReport({required Set<SubUserFilter> filters, required bool usingLoadingIdicator}) => TaskEither.tryCatch(() async {
-        final users = await GetIt.I<UserService>().getAllWithPaymentReport(filters);
-
-        subUsersWithPaymentReport = users;
-
-        return users;
-      }, (error, stackTrace) => error.toString());
-
-  TaskEither<String, void> fetchSubUsers(int pageIndex, int pageSize, String searchTerm) => TaskEither.tryCatch(() async {
-        final users = await GetIt.I<UserService>().getAll(pageIndex, pageSize, searchTerm, {});
-        _subUsers.addAll(users);
-
-        if (users.length < pageIndex) {
-          pagingState = PagingState<int, SubUserModel>(itemList: _subUsers);
-        } else {
-          pagingState = PagingState<int, SubUserModel>(nextPageKey: pageIndex + 1, itemList: users);
-        }
-
-        notifyListeners();
-      }, (error, stackTrace) => error.toString());
-
   TaskEither<String, SubUserModel> createUser(SubUserModel user) => TaskEither.tryCatch(
         () async => await GetIt.I<UserService>().createNew(user),
         (error, stackTrace) => error.toString(),
@@ -58,32 +25,22 @@ class SubUsersProvider extends PaginatedProvider<SubUserModel> with ChangeNotifi
       );
 
   void addUser(SubUserModel user) {
-    _subUsers.add(user);
+    items.add(user);
 
     notifyListeners();
-  }
-
-  @override
-  Future<void> refreshPagingState([String? newSearchTerm]) async {
-    if (newSearchTerm != null) {
-      _searchTerm = newSearchTerm;
-    }
-
-    _subUsers.clear();
-    await fetchSubUsers(0, Constants.pageSize, _searchTerm).run();
   }
 
   @override
   TaskEither<String, void> fetchData(int pageIndex, int pageSize, String searchTerm) => TaskEither.tryCatch(
         () async {
           final users = await GetIt.I<UserService>().getAll(pageIndex, pageSize, searchTerm, {});
-          _subUsers.addAll(users);
-          _searchTerm = searchTerm;
+          items.addAll(users);
+          this.searchTerm = searchTerm;
 
           if (users.length < pageIndex) {
-            pagingState = PagingState<int, SubUserModel>(itemList: _subUsers);
+            pagingState = PagingState<int, SubUserModel>(itemList: items);
           } else {
-            pagingState = PagingState<int, SubUserModel>(nextPageKey: pageIndex + 1, itemList: _subUsers);
+            pagingState = PagingState<int, SubUserModel>(nextPageKey: pageIndex + 1, itemList: items);
           }
 
           notifyListeners();
