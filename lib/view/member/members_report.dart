87d1af1fbe74817d952ac76910b5a31ba6e349c4
khangzxrr <khangzxrr@gmail.com>
2024-01-03 15:42:31 +0700

    remove report from provider using service instead

--- lib/view/member/members_report.dart
+++ lib/view/member/members_report.dart
@@ -1,23 +1,28 @@
+import 'dart:async';
 
+import 'package:after_layout/after_layout.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
 import 'package:hui_management/helper/constants.dart';
+import 'package:hui_management/model/sub_user_with_payment_report.dart';
 import 'package:hui_management/provider/sub_users_provider.dart';
-import 'package:provider/provider.dart';
+import 'package:hui_management/service/user_service.dart';
 import 'package:syncfusion_flutter_datagrid/datagrid.dart';
 
 import '../../grid_datasource/member_reports_datasource.dart';
 
-
 @RoutePage()
 class MemberReportScreen extends StatefulWidget {
-  const MemberReportScreen({super.key});
+  List<SubUserWithPaymentReport> reports = [];
+
+  MemberReportScreen({super.key});
 
   @override
   State<MemberReportScreen> createState() => _MemberReportScreenState();
 }
 
-class _MemberReportScreenState extends State<MemberReportScreen> {
+class _MemberReportScreenState extends State<MemberReportScreen> with AfterLayoutMixin<MemberReportScreen> {
   late MemberReportsDataSource dataSource;
 
   @override
@@ -29,8 +34,9 @@ class _MemberReportScreenState extends State<MemberReportScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final userWithPaymentReports = Provider.of<SubUsersProvider>(context, listen: true).subUsersWithPaymentReport;
-    dataSource.setReportsData(userWithPaymentReports);
+    dataSource.clearData();
+
+    dataSource.setReportsData(widget.reports);
 
     dataSource.sortedColumns.clear();
     dataSource.sortedColumns.add(const SortColumnDetails(name: 'name', sortDirection: DataGridSortDirection.ascending));
@@ -39,6 +45,7 @@ class _MemberReportScreenState extends State<MemberReportScreen> {
     bool isMobile = MediaQuery.of(context).size.width < Constants.smallScreenSize;
 
     return SfDataGrid(
+      allowPullToRefresh: true,
       frozenColumnsCount: 1,
       allowMultiColumnSorting: true,
       columnWidthMode: isMobile ? ColumnWidthMode.fitByCellValue : ColumnWidthMode.fill,
@@ -180,5 +187,17 @@ class _MemberReportScreenState extends State<MemberReportScreen> {
         ),
       ),
     ];
+  }
+
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) async {
+    try {
+      final fetchedReports = await GetIt.I<UserService>().getAllWithPaymentReport(0, 0, '', {SubUserFilter.AtLeastOnePayment});
+      setState(() {
+        widget.reports = fetchedReports;
+      });
+    } catch (e) {
+      print(e);
+    }
   }
 }
